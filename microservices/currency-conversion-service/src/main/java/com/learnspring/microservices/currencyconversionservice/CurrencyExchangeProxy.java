f029c7d0c243328f3af9ed5e44550df7dccd2ae4
Aishwarya Dubey <aishwaryadubey1234@gmail.com>
2021-07-19 18:02:46 +0530

    Registration Features

--- microservices/currency-conversion-service/src/main/java/com/learnspring/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ microservices/currency-conversion-service/src/main/java/com/learnspring/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 //name would be the name of the application for which we're creating the proxy;
-@FeignClient(name="currency-exchange", url="localhost:8000")     
+//@FeignClient(name="currency-exchange", url="localhost:8000") 
+
+@FeignClient(name="currency-exchange") // for using with eureka we don't need to add the url
 public interface CurrencyExchangeProxy {
 
 	@GetMapping(path="/currency-exchange/from/{from}/to/{to}")
