bf5677dbb5340450fa2be928b11b7b547223ad52
Aishwarya Dubey <aishwaryadubey1234@gmail.com>
2021-07-20 23:09:18 +0530

    Circuit Breaker

--- microservices/currency-exchange-service/src/main/java/com/learnspring/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ microservices/currency-exchange-service/src/main/java/com/learnspring/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,13 +1,27 @@
 package com.learnspring.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
+
 public class CircuitBreakerController {
 
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	@GetMapping("/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "hardCoded")
 	public String sampleApi() {
+		logger.info("Sample API Recieved");
+		new RestTemplate().getForEntity("http://localhost:8080/sampleapi", CircuitBreakerController.class);
 		return "Sample API";
+	}
+	
+	public String hardCoded(Exception ex) {
+		return "Hello ";
 	}
 }
